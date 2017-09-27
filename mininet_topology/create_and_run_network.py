a928251133bf02fcdac95fc520ba57fa81304571
shakram02 <ahmedhamdyau@gmail.com>
2017-09-27 16:42:32 +0200

    refactor main function, add parameters for create_network()

--- mininet_topology/create_and_run_network.py
+++ mininet_topology/create_and_run_network.py
@@ -23,12 +23,7 @@ def try_get_arg(index, key):
         return defaults[key]
 
 
-def create_network():
-    proxy_ip = try_get_arg(1, "proxy_ip")
-    proxy_port = int(try_get_arg(2, "proxy_port"))
-    switch_count = int(try_get_arg(3, "switch_count"))
-    hosts_per_switch = int(try_get_arg(4, "hosts_per_switch"))
-
+def create_network(proxy_ip="192.168.1.248", proxy_port=6833, switch_count=8, hosts_per_switch=2):
     net = Mininet()
     topo = ComplexTopo(net, switch_count, hosts_per_switch, proxy_ip, proxy_port)
     topo.build_network()
@@ -36,19 +31,31 @@ def create_network():
     return net
 
 
+def main():
+    global network
+
+    clean.cleanup()
+    setLogLevel('info')
+
+    proxy_ip = try_get_arg(1, "proxy_ip")
+    proxy_port = int(try_get_arg(2, "proxy_port"))
+    switch_count = int(try_get_arg(3, "switch_count"))
+    hosts_per_switch = int(try_get_arg(4, "hosts_per_switch"))
+
+    network = create_network(proxy_ip, proxy_port, switch_count, hosts_per_switch)
+    network.start()
+
+    print "[Showing connections]"
+    dumpNodeConnections(network.hosts)
+    print "[Testing network]"
+    network.pingAll()
+    network.stop()
+
+
 if __name__ == "__main__":
     network = None
     try:
-        clean.cleanup()
-        setLogLevel('info')
-        network = create_network()
-        network.start()
-
-        print "[Showing connections]"
-        dumpNodeConnections(network.hosts)
-        print "[Testing network]"
-        network.pingAll()
-        network.stop()
+        main()
     except KeyboardInterrupt:
         if network is not None:
             network.stop()
