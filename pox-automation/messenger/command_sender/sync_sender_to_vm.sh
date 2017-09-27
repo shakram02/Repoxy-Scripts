53945b8c2c11dfd21a1055af17c9631517ac3e85
shakram02 <ahmedhamdyau@gmail.com>
2017-09-27 21:33:27 +0200

    exclude hidden files

--- pox-automation/messenger/command_sender/sync_sender_to_vm.sh
+++ pox-automation/messenger/command_sender/sync_sender_to_vm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ip="192.168.1.241"
-rsync -ravI ssh . "mn0@$ip:/home/mn0/px/pox-automation/command_sender/"
+rsync -ravI ssh . --exclude=".*" "mn0@$ip:/home/mn0/px/pox-automation/command_sender/"
