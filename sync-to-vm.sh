648ffed0b132d60fcf61cd19095315bb13b93f59
shakram02 <ahmedhamdyau@gmail.com>
2017-10-16 04:49:33 +0200

    update sync path

--- sync-to-vm.sh
+++ sync-to-vm.sh
@@ -9,8 +9,6 @@ pass="mininet"
 ip="192.168.1.2$1"
 
 # Sync controller component
-#rsync -ravI ssh --exclude=".*"\
-#./controller-component/l2_all_to_controller.py \
-#"mn0@$ip:/home/mn0/pox/ext/"
+#rsync -ravI ssh ./controller-component/l2_all_to_controller.py "mn0@$ip:/home/mn0/pox/ext/"
 
-rsync -ravI ssh  --exclude=".*" . "mn0@$ip:/home/mn0/px/$(basename "$PWD")/"
+rsync -ravI ssh --exclude=".*" . "mn0@$ip:/home/mn0/px/$(basename "$PWD")/"
