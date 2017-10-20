4e760b0478d8879d208010289aeade4476b64a11
shakram02 <ahmedhamdyau@gmail.com>
2017-10-20 14:32:56 +0200

    timeout based interface

--- tests/test_library/messenger/controller_feedback_listener.py
+++ tests/test_library/messenger/controller_feedback_listener.py
@@ -2,28 +2,23 @@ import socket
 
 
 class ControllerFeedbackListener:
-    def __init__(self, listen_ip, listen_port, timeout):
+    def __init__(self, listen_ip, listen_port):
         self._ip = listen_ip
         self._port = listen_port
-        self._srver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._client = None
-        self._listening = False
-        self._srver_socket.settimeout(timeout)  # timeout for accepting
 
-    def start_listening(self):
+        self._srver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._srver_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._srver_socket.bind((self._ip, self._port))
         self._srver_socket.listen(1)
-        self._listening = True
 
-    def wait_for_client(self):
-        if not self._listening:
-            raise RuntimeError("Socket isn't listening, please call start_litening()")
-
+    def accept_client(self, timeout):
+        self._srver_socket.settimeout(timeout)  # timeout for accepting
         self._client, addr = self._srver_socket.accept()
         return addr
 
-    def wait_for_message(self, buffer_size=128):
+    def wait_for_message(self, timeout, buffer_size=128):
+        self._srver_socket.settimeout(timeout)  # timeout for accepting
         msg = self._client.recv(buffer_size).decode()
         return msg
 
