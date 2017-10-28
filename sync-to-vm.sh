b4ef5409d35ca564546950452391de0f15b25e8b
shakram02 <ahmedhamdyau@gmail.com>
2017-10-28 12:21:45 +0200

    sync all controller modules

--- sync-to-vm.sh
+++ sync-to-vm.sh
@@ -10,6 +10,6 @@ pass="mininet"
 ip="192.168.1.24$1"
 
 # Sync controller component
-#rsync -ravI ssh ./controller-component/l2_all_to_controller.py "mn0@$ip:/home/mn0/pox/ext/"
+#rsync -ravI ssh ./controller-component/*.py "mn0@$ip:/home/mn0/pox/ext/"
 
 rsync -ravI ssh --exclude=".*" . "mn0@$ip:/home/mn0/px/$(basename "$PWD")/"
