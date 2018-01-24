95d14dc665cd0ead26ab143de74241309782b210
shakram02 <ahmedhamdyau@gmail.com>
2018-01-24 14:39:46 +0200

    add colorize function

--- tests/entities/networking/utils.py
+++ tests/entities/networking/utils.py
@@ -13,3 +13,12 @@ def get_ip():
     finally:
         s.close()
     return ip
+
+
+BLUE_BACKGROUND_BRIGHT = "\033[0;104m"
+WHITE_BOLD = "\033[1;37m"
+RESET = "\033[0m"
+
+
+def colorize(string):
+    return "{}{}[{}]{}".format(BLUE_BACKGROUND_BRIGHT, WHITE_BOLD, string, RESET)
